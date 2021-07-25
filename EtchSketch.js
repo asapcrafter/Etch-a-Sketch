2db1e430198ac8d48f5b01b2b41bf7a0a13a0fc6
Steve <asapcrafter@gmail.com>
2021-07-24 22:29:21 -0700

    test

--- EtchSketch.js
+++ EtchSketch.js
@@ -2,36 +2,39 @@ const body = document.body;
 
 const flexGrid = document.querySelector(".grid");
 
-const d = document.createElement('div');
+//const d = document.createElement('div');
 
+//Grid creation 
 (function () {
     for (i = 1 ; i < (16*16) + 1; i++) {
         var d = document.createElement(`div`);
             d.setAttribute('id', `box`)
             addColor(d);
             flexGrid.appendChild(d);
-         d.innerHTML = `B-${i}`; 
+        // d.innerHTML = `B${i}`; 
     }
 }) ();
 
 function addColor(d) { 
     d.addEventListener('mouseover', event => {
-        event.target.style.color = "orange";
+        event.target.style.backgroundColor = "orange";
         event.stopPropagation();
         setTimeout(function() {
-            event.target.style.color = '';
+            event.target.style.backgroundColor = '';
         }, 500);
     });
 };
 
+const btnRestart = document.querySelector('#restartBtn');
+    btnRestart.onclick = () => {
+        resetCanvas();
 
-/*
-var test = document.getElementsByClassName('.grid');
-var numTest = test.length;
-  for (var i = 0 ; i < numTest; i++ ) {
-        test[0].addEventListener('click', function(event) {
-            event.target.text.color= "orange";
-            alert ('Hi');
-        });
     };
-    */
+
+function resetCanvas() {
+    var grid = document.querySelector('.grid');
+    var x = grid.querySelectorAll('#box');
+    x.style.backgroundColor = "white";
+}
+
+
