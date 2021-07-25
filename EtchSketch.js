4eb9bcc9e6fd617f87029d6e3cab99d9b1044f10
Steve <asapcrafter@gmail.com>
2021-07-24 20:48:05 -0700

    Added 'hover' effect for event

--- EtchSketch.js
+++ EtchSketch.js
@@ -1,15 +1,37 @@
 const body = document.body;
 
-const flexGrid = document.querySelector(".squares");
+const flexGrid = document.querySelector(".grid");
 
 const d = document.createElement('div');
 
 (function () {
-    for (i = 0 ; i < (16*16) ; i++) {
-        var d = document.createElement('div');
-         flexGrid.appendChild(d);
-         d.innerHTML = "Hi"; 
+    for (i = 1 ; i < (16*16) + 1; i++) {
+        var d = document.createElement(`div`);
+            d.setAttribute('id', `box`)
+            addColor(d);
+            flexGrid.appendChild(d);
+         d.innerHTML = `B-${i}`; 
     }
 }) ();
- //   boxes.appendChild(d);
-  //  d.innerHTML="Hello"
+
+function addColor(d) { 
+    d.addEventListener('mouseover', event => {
+        event.target.style.color = "orange";
+        event.stopPropagation();
+        setTimeout(function() {
+            event.target.style.color = '';
+        }, 500);
+    });
+};
+
+
+/*
+var test = document.getElementsByClassName('.grid');
+var numTest = test.length;
+  for (var i = 0 ; i < numTest; i++ ) {
+        test[0].addEventListener('click', function(event) {
+            event.target.text.color= "orange";
+            alert ('Hi');
+        });
+    };
+    */
